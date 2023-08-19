3df15eab7e4353bd3051567b92895c17f75727d8
jaggerharness <jagger.dev@gmail.com>
2023-08-19 14:06:21 -0500

    chore: Seed initial data into db

--- HotelListing.API/Migrations/20230819190119_SeededCountriesAndHotels.Designer.cs
+++ HotelListing.API/Migrations/20230819190119_SeededCountriesAndHotels.Designer.cs
@@ -3,6 +3,7 @@ using HotelListing.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelListing.API.Migrations
 {
     [DbContext(typeof(HotellistingDbContext))]
-    partial class HotellistingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230819190119_SeededCountriesAndHotels")]
+    partial class SeededCountriesAndHotels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
