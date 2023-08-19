c96351f02ef4d82d1ee60ed8026d94171d3baecd
jaggerharness <jagger.dev@gmail.com>
2023-08-19 00:31:29 -0500

    Begin db connection

---
+++ HotelListing.API/Data/Class.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotellistingDbContext : DbContext
+    {
+        public HotellistingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
